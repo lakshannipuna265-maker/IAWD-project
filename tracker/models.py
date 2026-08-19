7125af968a9132d2de664ab1b038a82250f777c8
lakshannipuna265-maker <lakshannipuna265@gmail.com>
2026-08-19 20:19:28 +0530

    Add Category model with migration and admin registration

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Model(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title   
+
+class Category(models.Model):
+    CATEGORY_TYPES = [
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    category_type = models.CharField(max_length=20, choices=CATEGORY_TYPES)
+
+    def __str__(self):
+        return self.name
