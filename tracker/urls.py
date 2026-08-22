216b9cbd9107bc54e2a2a17ea419c17fc5728cef
lakshannipuna265-maker <lakshannipuna265@gmail.com>
2026-08-22 09:39:20 +0530

    Add Category list view, form, template, and URL

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('categories/', views.category_list, name='category_list'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Category
+from .forms import CategoryForm     
 
 def dashboard(request):
     return render(request, 'tracker/dashboard.html')
+
+@login_required
+def category_list(request): 
+    categories = Category.objects.filter(user=request.user)
+    return render(request, 'tracker/category_list.html', {'categories': categories})
