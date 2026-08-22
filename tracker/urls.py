6b847892edb4e623bdce7a66f43f19d6ed47d4d1
lakshannipuna265-maker <lakshannipuna265@gmail.com>
2026-08-22 12:38:48 +0530

    Add Category update and delete views, templates, and URLs

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('categories/', views.category_list, name='category_list'),
+    path('categories/add/', views.category_create, name='category_create'),
+    path('categories/<int:pk>/edit/', views.category_update, name='category_update'),
+    path('categories/<int:pk>/delete/', views.category_delete, name='category_delete'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Category
 from .forms import CategoryForm     
@@ -10,3 +10,36 @@ def dashboard(request):
 def category_list(request): 
     categories = Category.objects.filter(user=request.user)
     return render(request, 'tracker/category_list.html', {'categories': categories})
+
+@login_required
+def category_create(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category = form.save(commit=False)
+            category.user = request.user
+            category.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm()
+    return render(request, 'tracker/category_form.html', {'form': form})
+
+@login_required
+def category_update(request, pk):
+    category = get_object_or_404(Category, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'tracker/category_form.html', {'form': form})
+
+@login_required
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk, user=request.user)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('category_list')
+    return render(request, 'tracker/category_confirm_delete.html', {'category': category})
