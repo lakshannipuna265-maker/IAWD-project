4afb81b803378c320fb2e18edcef966b5d81d02e
lakshannipuna265-maker <lakshannipuna265@gmail.com>
2026-08-22 09:08:26 +0530

    Wire up Django auth login/logout URLs and template

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('tracker.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
